26ac66553af41b7c7851636baee1dd2ecd02c422
Alina <alinaami@mail.ru>
2019-08-11 20:14:00 +0300

    Create LoginTest.cs

---
+++ LoginTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SelenuimInitial
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+        [SetUp]
+        public void start()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--no-sandbox");
+            _driver = new ChromeDriver(options);
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+        }
+
+        [TestMethod]
+        public void LoginToShopTest()
+        {
+            _driver.Navigate().GoToUrl("http://localhost/litecart/admin/");
+            var loginField = _driver.FindElement(By.Name("username"));
+            loginField.SendKeys("admin");
+            var passwordField = _driver.FindElement(By.Name("password"));
+            passwordField.SendKeys("admin");
+            var loginButton = _driver.FindElement(By.Name("login"));
+            loginButton.Click();
+            _wait.Until(el => el.FindElement(By.ClassName("notice success")).Displayed);
+        }
+        [TearDown]
+        public void stop()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
